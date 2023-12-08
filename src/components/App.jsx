942f794ff8cc6f502a3b6c19dc1ad4642b9fcc97
MariiaSam <kontragent@ukr.net>
2023-12-08 09:11:34 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { VideoList } from 'components/VideoList/VideoList';
+import { Player } from 'components/Player/Player';
+import videos from '../videos.json';
+
+ class App extends Component {
+  state = {
+    selectedVideo: null,
+  };
+
+  selectVideo = link => {
+    this.setState({ selectedVideo: link });
+  };
+
+  render() {
+    return (
+      <div style={{ padding: 24 }}>
+        <h1>Selected video: {this.state.selectedVideo}</h1>
+        <VideoList videos={videos} onSelect={this.selectVideo} />
+        <Player url={this.state.selectedVideo} />
+      </div>
+    );
+  }
+}
+
+export default App;
